c236c8f9ae1cec3ae180e4047f0f373ea7d639ff
chiemc <chevychiem@gmail.com>
2025-09-23 00:38:08 -0400

    created PostView and moved code from ContentView to PostView

--- demo01-instagram/ContentView.swift
+++ demo01-instagram/ContentView.swift
@@ -9,42 +9,12 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack(alignment: .leading) {
-            HStack {
-                Image("Image")
-                    .resizable()
-                    .frame(width: 44, height: 44)
-                    .clipShape(Circle())
-                Text("chevychiem")
-                    .font(.headline)
-                Spacer()
-                Image(systemName: "ellipsis")
-            }
-            Image("IMG_6028")
-                .resizable()
-                .aspectRatio(1, contentMode: .fit)
-                .padding(.horizontal, -16)
-            HStack {
-                Image(systemName: "heart.fill")
-                Text("100.3M")
-                Image(systemName: "bubble")
-                Text("99.2M")
-                Image(systemName: "repeat")
-                Text("5.9M")
-                Image(systemName: "paperplane")
-                Text("58.2M")
-                Spacer()
-                Image(systemName: "bookmark.fill")
-            }
-            HStack {
-                Text("chevychiem")
-                    .font(.headline)
-                Text("hey lol")
-            }
-            Text("30 seconds ago")
-                .font(.caption)
-                .foregroundColor(.secondary)
-        } .padding(10)
+        HStack {
+            Image(systemName: "person.circle")
+                .foregroundStyle(.blue)
+            Text("Chevy Chiem")
+                .bold()
+        }
     }
 }
 
